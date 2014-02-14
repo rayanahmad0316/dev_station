f8a8cb2ab9282d5ebf92abe8b54b2051239601fe
Warren Smith <warren.smith@sendoutcards.com>
2014-02-14 11:36:25 -0700

    Bugfix.

--- bin/exid.sh
+++ bin/exid.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 SSH_SPEC_TARGET=$1
-PATH_SSH_DIR=~/.ssh
-PATH_LOCAL_ID=$PATH_SSH_DIR/id_rsa.pub
-PATH_ID_STORE=$PATH_SSH_DIR/authorized_keys
+PATH_LOCAL_ID=id_rsa.pub
+PATH_ID_STORE=authorized_keys
 
-cat $PATH_LOCAL_ID | ssh $SSH_SPEC_TARGET "mkdir -p -m 700 $PATH_SSH_DIR && touch $PATH_ID_STORE && chmod 600 $PATH_ID_STORE && cat - >> $PATH_ID_STORE"
+cat ~/.ssh/$PATH_LOCAL_ID | ssh $SSH_SPEC_TARGET "mkdir -p -m 700 ~/.ssh && touch ~/.ssh/$PATH_ID_STORE && chmod 600 ~/.ssh/$PATH_ID_STORE && cat - >> ~/.ssh/$PATH_ID_STORE"
