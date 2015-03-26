9d6bce28e6975e687717bbc85d15699a05488994
Warren Smith <warren.smith@sendoutcards.com>
2015-03-26 08:55:29 -0600

    Tweak boot2docker_wrapper.sh output.

--- bin/boot2docker_wrapper.sh
+++ bin/boot2docker_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "`boot2docker status`" != "running" ]; then
-    boot2docker start
+    boot2docker start 2> /dev/null | grep -v export | grep -v connect
 fi
 $(boot2docker shellinit 2> /dev/null)
 docker $*
