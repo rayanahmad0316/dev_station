e8869563377a2e7aa8f6b0fbe64ed28cd3615935
Warren Smith <wsmith@summitesp.com>
2015-09-25 15:42:10 -0500

    Change layout for new Summit ESP dev environment.

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -81,38 +81,52 @@ bindKeysAdjustWindow({ "cmd" }, { "3", "pad3" }, 4, 4, 4, 4)
 bindKeysAdjustWindow({ "cmd" }, { "2", "pad2" }, 0, 4, 8, 4)
 
 function layout()
-    local laptopScreen = hs.screen.findByName("Color LCD")
-    local desktopScreen = hs.screen.findByName("S27B550")
-    local comsScreen = laptopScreen or desktopScreen
-    local mainScreen = comsScreen:toEast() or comsScreen
-    local secondaryScreen = mainScreen:toEast() or mainScreen
+    local centerScreen = hs.screen.primaryScreen()
+    local rightScreen = centerScreen:toEast()
+    local leftScreen = centerScreen:toWest()
+
+    local commsScreen = centerScreen
+    local toolsScreen = centerScreen
+    local codeScreen = centerScreen
+
+    if centerScreen and rightScreen and leftScreen then
+        -- Work
+        commsScreen = rightScreen
+        toolsScreen = centerScreen
+        codeScreen = leftScreen
+    elseif centerScreen and rightScreen then
+        -- Home
+        commsScreen = rightScreen
+        toolsScreen = rightScreen
+        codeScreen = centerScreen
+    end
 
     hs.layout.apply({
         --Communications / Administration Apps
-        { "Calendar", nil, comsScreen, hs.geometry.rect(0, 0, 1, 1), nil, nil },
-        { "Mail", nil, comsScreen, hs.geometry.rect(0, 0, 0.5, 1), nil, nil },
-        { "Messages", nil, comsScreen, hs.geometry.rect(0.5, 0, 0.5, 1), nil, nil },
-        { "Slack", nil, comsScreen, hs.geometry.rect(0.5, 0, 0.5, 1), nil, nil },
+        { "Calendar", nil, commsScreen, hs.geometry.rect(0, 0, 1, 1), nil, nil },
+        { "Mail", nil, commsScreen, hs.geometry.rect(0, 0, 0.5, 1), nil, nil },
+        { "Messages", nil, commsScreen, hs.geometry.rect(0.5, 0, 0.5, 1), nil, nil },
+        { "Slack", nil, commsScreen, hs.geometry.rect(0.5, 0, 0.5, 1), nil, nil },
 
         -- Misc Developer Tools
         -- Left 3/8
-        { "Terminal", nil, mainScreen, hs.geometry.rect(0, 0, 0.375, 1), nil, nil },
+        { "Terminal", nil, toolsScreen, hs.geometry.rect(0, 0, 0.375, 1), nil, nil },
         -- Right 5/8
-        { "Google Chrome", nil, mainScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
-        { "SourceTree", nil, mainScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
-        { "MySQLWorkbench", nil, mainScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
-        { "iTunes", "iTunes", mainScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
+        { "Google Chrome", nil, toolsScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
+        { "SourceTree", nil, toolsScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
+        { "MySQLWorkbench", nil, toolsScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
+        { "iTunes", "iTunes", toolsScreen, hs.geometry.rect(0.375, 0, 0.625, 1), nil, nil },
 
         -- Put the iTunes miniplayer in the bottom right corner
         -- in the unused space not taken by the dock
-        { "iTunes", "MiniPlayer", mainScreen, nil, nil, hs.geometry.rect(-350, -45, 350, 45) },
+        { "iTunes", "MiniPlayer", centerScreen, nil, nil, hs.geometry.rect(-350, -45, 350, 45) },
 
         -- Coding IDE gets a screen all to itself
-        { "PyCharm", nil, secondaryScreen, hs.geometry.rect(0, 0, 1, 1), nil, nil },
+        { "PyCharm", nil, codeScreen, hs.geometry.rect(0, 0, 1, 1), nil, nil },
     })
 
     hs.layout.apply({
-        { "Google Chrome", "^Developer Tools.*$", mainScreen, hs.geometry.rect(0, 0, 0.375, 1), nil, nil },
+        { "Google Chrome", "^Developer Tools.*$", toolsScreen, hs.geometry.rect(0, 0, 0.375, 1), nil, nil },
     }, string.match)
 
     for index, win in pairs(hs.window.visibleWindows()) do
